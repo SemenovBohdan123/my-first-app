bd232650793f1edc358e9bd5bbe95cd98cd272de
BohdanSemenov123 <nadgbosemenov@gmail.com>
2021-09-13 19:48:29 +0300

    changes button variant

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" color="secondary">
+      <Button variant="outlined" color="secondary">
         Primary
       </Button>
     </div>
